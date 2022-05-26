4d10d11823c1726a6ca341779c8ae91b80a153ab
sugawara <sugawara@ssmacmini20.local>
2022-05-27 08:01:33 +0900

    fh

--- R/meansTest.R
+++ R/meansTest.R
@@ -10,6 +10,7 @@
 #' @param sd2 standard deviation of group 2
 #' @param nd2 number of group 2
 #' @return Deviation Deviation of 2 mean values
+#' @return Cohens_d Cohens-d
 #' @export
 #' @examples
 #' # meansTest(60, 10, 20, 55, 15, 25)
