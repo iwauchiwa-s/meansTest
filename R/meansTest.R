77c74293eaa754be3032acae36dbe0554923676b
sugawara <sugawara@ssmacmini20.local>
2022-05-27 08:12:48 +0900

    hh

--- R/meansTest.R
+++ R/meansTest.R
@@ -9,7 +9,7 @@
 #' @param av2 mean value of group 2
 #' @param sd2 standard deviation of group 2
 #' @param nd2 number of group 2
-#' @return Stats test
+#' @return Deviation Test
 #' @export
 #' @examples
 #' # meansTest(60, 10, 20, 55, 15, 25)
